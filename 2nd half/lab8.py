661adf99f25bdc17b10f87510783da8dbe22aaba
zhabaa <vova.dovnar2006@gmail.com>
2026-02-17 11:58:26 +0300

    update 7, 8 labs && update readme.md

--- 2nd half/lab8.py
+++ 2nd half/lab8.py
@@ -1,23 +1,26 @@
-import sys
+def change_coins(amount: int, coins: list[int]) -> int:
+    if amount < 0:
+        return 0
 
-amount = 5
-coins = [1, 2, 5]
+    coins = sorted(set(coins))
 
-if amount < 0:
-    print(0)
-    sys.exit(1)
+    dp = [0] * (amount + 1)
+    dp[0] = 1
 
-coins = sorted(set(coins))
-dp = [0] * (amount + 1)
-dp[0] = 1
+    for c in coins:
+        for i in range(c, amount + 1):
+            dp[i] += dp[i - c]
 
-for c in coins:
-    for x in range(c, amount + 1):
-        dp[x] += dp[x - c]
+    return dp[amount]
 
-print(dp[amount])
+
+amount = 100
+coins = [1, 2, 5, 10]
+
+print(change_coins(amount, coins))
+
 # 4
 # 5 = 5
-# 5 = 2+2+1
-# 5 = 2+1+1+1
-# 5 = 1+1+1+1+1
+# 5 = 2 + 2 + 1 
+# 5 = 2 + 1 + 1 + 1
+# 5 = 1 + 1 + 1 + 1 + 1
